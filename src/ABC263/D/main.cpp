e57e7fc3f81563f02552297350e7cd28b7b93ce8
Tomonori Yamada <kuronekodai15@gmail.com>
2022-08-06 23:02:00 +0900

    practice 8/6

---
+++ src/ABC263/D/main.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <cstring>
+#include <cstdio>
+#include <algorithm>
+#include <queue>
+#include <cmath>
+#include <limits>
+#include <map>
+#include <vector>
+#include <set>
+#include <iomanip>
+using namespace std;
+#define rep(i, n) for (int i = 0; i < (int)(n); i++)
+typedef long long ll;
+
+int main() {
+  int n, l, r;
+  
+  return 0;
+}
